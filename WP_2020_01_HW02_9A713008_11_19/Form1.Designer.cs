299951d288b47f3008e5dd5f7f9e7d9ca91be0be
張秤羿 <badthing0102@gmail.com>
2020-11-18 10:39:58 +0800

    加入專案檔案。

---
+++ WP_2020_01_HW02_9A713008_11_19/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace WP_2020_01_HW02_9A713008_11_19
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.pB1 = new System.Windows.Forms.PictureBox();
+            this.rTB1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pB1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("微軟正黑體", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn1.Location = new System.Drawing.Point(122, 48);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(170, 115);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "按";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // pB1
+            // 
+            this.pB1.Image = global::WP_2020_01_HW02_9A713008_11_19.Properties.Resources.P1;
+            this.pB1.Location = new System.Drawing.Point(122, 196);
+            this.pB1.Name = "pB1";
+            this.pB1.Size = new System.Drawing.Size(170, 221);
+            this.pB1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pB1.TabIndex = 1;
+            this.pB1.TabStop = false;
+            // 
+            // rTB1
+            // 
+            this.rTB1.Font = new System.Drawing.Font("微軟正黑體", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rTB1.Location = new System.Drawing.Point(395, 101);
+            this.rTB1.Name = "rTB1";
+            this.rTB1.Size = new System.Drawing.Size(167, 156);
+            this.rTB1.TabIndex = 2;
+            this.rTB1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rTB1);
+            this.Controls.Add(this.pB1);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pB1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.PictureBox pB1;
+        private System.Windows.Forms.RichTextBox rTB1;
+    }
+}
+
---
+++ WP_2020_01_HW02_9A713008_11_19/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WP_2020_01_HW02_9A713008_11_19.Properties;
+
+
+namespace WP_2020_01_HW02_9A713008_11_19
+{
+    public partial class Form1 : Form
+    {
+        List<Image> list = new List<Image>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            Random a = new Random();//亂數種子
+            int i = a.Next(0, 52);
+            pB1.Image = list[i];
+        }
+        #region 生命週期
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            #region 撲克牌
+            
+            list.Add(Resources.P1);
+            list.Add(Resources.P2);
+            list.Add(Resources.P3);
+            list.Add(Resources.P4);
+            list.Add(Resources.P5);
+            list.Add(Resources.P6);
+            list.Add(Resources.P7);
+            list.Add(Resources.P8);
+            list.Add(Resources.P9);
+            list.Add(Resources.P10);
+            list.Add(Resources.P11);
+            list.Add(Resources.P12);
+            list.Add(Resources.P13);
+            list.Add(Resources.P14);
+            list.Add(Resources.P15);
+            list.Add(Resources.P16);
+            list.Add(Resources.P17);
+            list.Add(Resources.P18);
+            list.Add(Resources.P19);
+            list.Add(Resources.P20);
+            list.Add(Resources.P21);
+            list.Add(Resources.P22);
+            list.Add(Resources.P23);
+            list.Add(Resources.P24);
+            list.Add(Resources.P25);
+            list.Add(Resources.P26);
+            list.Add(Resources.P27);
+            list.Add(Resources.P28);
+            list.Add(Resources.P29);
+            list.Add(Resources.P30);
+            list.Add(Resources.P31);
+            list.Add(Resources.P32);
+            list.Add(Resources.P33);
+            list.Add(Resources.P34);
+            list.Add(Resources.P35);
+            list.Add(Resources.P36);
+            list.Add(Resources.P37);
+            list.Add(Resources.P38);
+            list.Add(Resources.P39);
+            list.Add(Resources.P40);
+            list.Add(Resources.P41);
+            list.Add(Resources.P42);
+            list.Add(Resources.P43);
+            list.Add(Resources.P44);
+            list.Add(Resources.P45);
+            list.Add(Resources.P46);
+            list.Add(Resources.P47);
+            list.Add(Resources.P48);
+            list.Add(Resources.P49);
+            list.Add(Resources.P50);
+            list.Add(Resources.P51);
+            list.Add(Resources.P52);
+            #endregion
+        }
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+        #endregion
+
+
+
+    }
+}
